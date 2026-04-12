f060a54d2904c07a25ee111f500138d1dc1168f9
Madelyne Bautista <mbautistam6@miumg.edu.gt>
2026-04-11 22:42:11 -0600

    Add files via upload

--- Proyecto_ZooA.java
+++ Proyecto_ZooA.java
@@ -2,30 +2,69 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.proyecto_zooa;
+package poo.proyecto_zoo.java;
 
 /**
  *
- * @author Daniel
+ * @author madel
  */
-public class Proyecto_ZooA {
-    //Aqui lo que hacemos es realizar un recorrido por la matriz animal para mostrar los animales que se encuentran en ella
-     public static void mostrarAnimales(Animal[] animales) {
-        for (int i = 0; i < contador; i++) {
-            System.out.println(animales[i].nombre + " - " + animales[i].edad + " anios");
-            animales[i].alimentar();   
-            //Lo que pensamos aqui  seria bueno que al mostrar los animales podriamos alimentarlos de una vez
-        }
-    }
-     //Aqui solicitamos al usuario que nos proporcione los dias para poder realizar el calculo del consumo del animal
-    public static void calcularConsumo(Scanner sc, Animal[] animales, int contador) {
-    System.out.print("Ingrese dias: ");
-    int dias = sc.nextInt();
+import java.util.Scanner;
 
-    for (int i = 0; i < contador; i++) {  // solo recorre animales que existen
-        double total = animales[i].calcularConsumo(dias);
-        System.out.println(animales[i].nombre + " necesita " + total + " de comida " + dias + " dias");
-        //Esta parte lo que nos permite es mostrarle el alimento necesario para los dias dados por el usuario 
+public class Proyecto_ZooA {
+
+    static int contador = 0; 
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Animal[] animales = new Animal[3];
+
+        int opcion;
+        do {
+            System.out.println("------BIENVENIDOS AL ZOOLOGICO LA AURORA-----");
+            System.out.println("Comencemos...");
+            System.out.println("1. Zoo");
+            System.out.println("2.  Calcular comida ");
+            System.out.println("3. cvs");
+            System.out.println("4. Salir");
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    //menuZoo(sc, animales);
+                    break;
+                case 2:
+                   //calcularConsumo(sc, animales, contador);
+                   break;
+                case 4:
+                    System.out.println("Adios...");
+                    break;
+                default:
+                    System.out.println("Opcion invalida");
+            }
+        } while (opcion != 4);
     }
-}
+
+    public static void menuZoo(Scanner sc, Animal[] animales) {
+        int opcion;
+
+        do {
+            System.out.println("\n--- ZOOLOGICO LA AURORA  ---");
+            System.out.println("1. Agregar animal");
+            System.out.println("2. Ver animales");
+            System.out.println("3. Regresar");
+            System.out.print("Opcion: ");
+
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    //agregarAnimal(sc, animales); 
+                    break;
+                case 2:
+                    //mostrarAnimales(animales); 
+                    break;
+            }
+
+        } while (opcion != 3);
+    }
 }
