24abd14ac239e77f90ebf352c085db48dadb7095
Kevinloopez <klopezc70@miumg.edu.gt>
2026-04-11 23:30:21 -0600

    Add files via upload

--- Proyecto_ZooA.java
+++ Proyecto_ZooA.java
@@ -1,13 +1,9 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
+package proyecto_zooa;
 
-package poo.proyecto_zoo.java;
-
-/**
- *
- * @author madel
- */
 import java.util.Scanner;
 
 public class Proyecto_ZooA {
@@ -30,10 +26,13 @@ public class Proyecto_ZooA {
 
             switch (opcion) {
                 case 1:
-                    //menuZoo(sc, animales);
+                    menuZoo(sc, animales);
                     break;
                 case 2:
-                   //calcularConsumo(sc, animales, contador);
+                   calcularConsumo(sc, animales, contador);
+                   break;
+                case 3:
+                   exportarCSV(animales);
                    break;
                 case 4:
                     System.out.println("Adios...");
@@ -58,13 +57,91 @@ public class Proyecto_ZooA {
 
             switch (opcion) {
                 case 1:
-                    //agregarAnimal(sc, animales); 
+                    agregarAnimal(sc, animales); 
                     break;
                 case 2:
-                    //mostrarAnimales(animales); 
+                    mostrarAnimales(animales); 
                     break;
             }
 
         } while (opcion != 3);
     }
+
+    public static void agregarAnimal(Scanner sc, Animal[] animales) {
+
+        if (contador >= 3) {
+            System.out.println("Ya no se puede agregar mas animales.");
+            return;
+        }
+
+        System.out.println("1. Mamifero"); //
+        System.out.println("2. Ave");
+        System.out.println("3. Reptil");
+
+        int tipo = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Nombre: ");
+        String nombre = sc.nextLine();
+
+        System.out.print("Edad: ");
+        int edad = sc.nextInt();
+
+        System.out.print("comida:  "); 
+        double comida = sc.nextDouble();
+
+        if (tipo == 1) {
+            animales[contador] = new Mamifero(nombre, edad, comida);
+        } else if (tipo == 2) {
+            animales[contador] = new Ave(nombre, edad, comida);
+        } else if (tipo == 3) {
+            animales[contador] = new Reptil(nombre, edad, comida);
+        } else {
+            System.out.println("Tipo invalido");
+            return;
+        }
+
+        contador++; 
+    }
+
+    public static void mostrarAnimales(Animal[] animales) {
+
+        for (int i = 0; i < contador; i++) {
+            System.out.println(animales[i].nombre + " - " + animales[i].edad + " anios");
+            animales[i].alimentar(); 
+        }
+    }
+    public static void calcularConsumo(Scanner sc, Animal[] animales, int contador) {
+    System.out.print("Ingrese dias: ");
+    int dias = sc.nextInt();
+
+    for (int i = 0; i < contador; i++) {  // solo recorre animales que existen
+        double total = animales[i].calcularConsumo(dias);
+        System.out.println(animales[i].nombre + " necesita " + total + " de comida " + dias + " dias");
+    }
 }
+
+public static void exportarCSV(Animal[] animales) {
+    try {
+        java.io.FileWriter writer = new java.io.FileWriter("animales.csv");
+
+       
+        writer.write("Nombre,  Edad, ConsumoDiario\n");
+
+        
+       for (int i = 0; i < Proyecto_ZooA.contador; i++) {
+            writer.write(animales[i].nombre + ", " +
+                         animales[i].edad + ", " +
+                         animales[i].consumoDiario + "\n");
+        }
+
+        writer.close();
+        System.out.println("Datos exportados a animales.csv correctamente");
+
+    } catch (Exception e) {
+        System.out.println("Error al exportar CSV");
+    }
+}
+}
+
+   
