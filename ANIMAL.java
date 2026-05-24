368d38be05b7bdd61f67ea4b79612a9718dc03d1
Erick-Alvarado64 <ealvaradoo9@miumg.edu.gt>
2026-05-23 19:43:15 -0600

    Update ANIMAL.java

--- ANIMAL.java
+++ ANIMAL.java
@@ -1,28 +1,58 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+package proyecto_zooa;
 
-/**
- *
- * @author tonoc
- */
-public class ANIMAL {
+public class Animal {
     
-    String nombre;
-    int edad;
-    double consumoDiario;
+   
+    protected String nombre;
+    protected long idAnimal;
+    protected int edad;
+    protected double consumoDiario;
 
-    public Animal(String nombre, int edad, double consumoDiario) {
+
+    public Animal(long idAnimal, String nombre, int edad, double consumoDiario) {
+        this.idAnimal = idAnimal;
         this.nombre = nombre;
         this.edad = edad;
         this.consumoDiario = consumoDiario;
     }
 
+    public long getIdAnimal() {
+        return idAnimal;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public double getConsumoDiario() {
+        return consumoDiario;
+    }
+
+    public void setIdAnimal(long idAnimal) {
+        this.idAnimal = idAnimal;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setConsumoDiario(double consumoDiario) {
+        this.consumoDiario = consumoDiario;
+    }
+
     public void mostrarInfo() {
         System.out.println("Nombre: " + nombre);
-        System.out.println("Edad: " + edad);
-        System.out.println("Consumo diario: " + consumoDiario);
+        System.out.println("Edad: " + edad + " anios");
+        System.out.println("Consumo diario: " + consumoDiario + " lbs");
+        System.out.println();
     }
 
     public double calcularConsumo(int dias) {
@@ -32,8 +62,17 @@ public class ANIMAL {
             return consumoDiario + calcularConsumo(dias - 1);
         }
     }
-     public void alimentar() {
+
+    public void alimentar() {
         System.out.println(nombre + " esta comiendo.");
     }
 
+    @Override
+    public String toString() {
+        return "ID: " + idAnimal +
+               ", Nombre: " + nombre +
+               ", Edad: " + edad +
+               ", Consumo diario: " + consumoDiario;
+    }
 }
+
